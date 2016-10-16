7d4b1d66733fc234f9b4eefe9494398e3860ce39
Joni Weiss <joni@handmadepixels.com>
2016-10-15 21:01:00 -0600

    feat(core) Add function to switch pos/neg numbers

--- js/calc.js
+++ js/calc.js
@@ -1,50 +1,21 @@
 "use strict";
 console.clear();
-console.log("Howdy! Welcome to Calc");
+console.log("Begin:", document.title);
 
 let formInputStr = ''; //Set initial
-let formInputArr = []; //Set initial
+// let formInputArr = []; //Set initial
 
 function updateDisplay (str,updType) {
   if ( updType === "append") {
-    console.log("true");
     document.querySelector('#display').innerHTML += str;
-  } else {
-    console.log("false");
+  } else { //updType != "append"
     document.querySelector('#display').innerHTML = str;
   }
   return true;
 }
 
+//Init Display
 updateDisplay(0,"replace");
-
-// function myOnClick(id) {
-//   let inputStr;
-//   let btnType;
-//   switch (inputStr) {
-//     case [0123456789].indexOf(inputStr):
-//       btnType = num;
-//       inputStr = id;
-//       formInputStr += numStr;
-//       if (typeof formInputArr[formInputArr.length-1] !== 'string') {
-//         console.log("num: " formInputArr[formInputArr.length-1]);
-//       }
-//       console.log("onNumClick:",numStr,".");
-//       break;
-//     default:
-//       btnType = operator;
-//       inputStr = ' ' + id + ' ';
-//       formInputStr +=  inputStr;
-//       console.log("onOpClick:",inputStr,".");
-//   }
-//
-//   if (formInputArr.length !== 0) {
-//
-//   } else { //Array is empty - load 1st element
-//     formInputArr.push(inputStr);
-//   }
-//
-// }
 
 function onNumClick(id) {
   if (formInputStr.length === 0) {
@@ -52,25 +23,38 @@ function onNumClick(id) {
   }
   let numStr = id;
   formInputStr += numStr;
-  console.log("onNumClick:",numStr,".");
   updateDisplay(numStr, "append");
 }
 
 function onOpClick(id) {
   let opStr = ' ' + id + ' ';
   formInputStr +=  opStr;
-  console.log("onOpClick:",opStr,".");
   updateDisplay(opStr, "append");
+}
+
+function switchPosNeg(id) {
+  let inputArr = formInputStr.split(' ');
+  let lastItem = inputArr.length - 1;
+
+  let numStr = Number(inputArr[inputArr.length-1]);
+
+  numStr *= -1;
+
+  inputArr[inputArr.length-1] = numStr;
+  formInputStr = inputArr.join(' ');
+
+  // Add extra space at end of string for
+  updateDisplay(formInputStr, "replace");
 }
 
 function onEquals() {
   let inputArr = formInputStr.split(' ');
-  let n1 = parseFloat(inputArr[0]);
-  let n2 = parseFloat(inputArr[2]);
+  formInputStr = ''; // Reset for next calc
+  let n1 = Number(inputArr[0]);
+  let n2 = Number(inputArr[2]);
   let op = inputArr[1];
   //calculator (num1, num2, cb)
   let result = calculator(n1,n2,op);
-  console.log("n1: ", n1, " n2: ", n2, " op: ", op, "result:",result);
   updateDisplay(result, "replace");
 }
 
@@ -82,6 +66,10 @@ function onClear () {
   formInputStr = inputArr.join(' ') + ' ';
   updateDisplay(formInputStr, "replace");
 }
+
+function calculator (num1, num2, cb) {
+  return window[cb](num1,num2);
+};
 
 function add (num1, num2) {
   return num1 + num2;
@@ -105,8 +93,4 @@ function mod (num1, num2) {
 
 function pct (num1, num2) {
   return num1 % num2;
-};
-
-function calculator (num1, num2, cb) {
-  return window[cb](num1,num2);
 };
